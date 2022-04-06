0fc792cb84708afc49fd7944b51233d37a56a96a
Francisco <fvcneto.master98@gmail.com>
2022-04-06 19:40:15 -0300

    first godot game (tutorial)

---
+++ Main.gd
@@ -0,0 +1,65 @@
+extends Node
+
+export(PackedScene) var mob_scene
+var score
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func new_game():
+	get_tree().call_group("mobs", "queue_free")
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$Music.play()
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+
+
+func _on_MobTimer_timeout():
+	var mob = mob_scene.instance()
+	
+	var mob_spaw_location = get_node("MobPath/MobSpawnLocation")
+	mob_spaw_location.offset = randi()
+	
+	var direction = mob_spaw_location.rotation + PI / 2
+	
+	mob.position = mob_spaw_location.position
+	
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
